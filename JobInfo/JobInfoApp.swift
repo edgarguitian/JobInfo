a2db915f4e2f698a6501b04007cd9005e868245e
Edgar <edguitian@imagine800.es>
2024-12-19 16:44:13 +0100

    Initial Commit

---
+++ JobInfo/JobInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobInfoApp.swift
+//  JobInfo
+//
+//  Created by Edgar Guitian Rey on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct JobInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobInfoTests/JobInfoTests.swift
@@ -0,0 +1,16 @@
+//
+//  JobInfoTests.swift
+//  JobInfoTests
+//
+//  Created by Edgar Guitian Rey on 19/12/24.
+//
+
+import Testing
+
+struct JobInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
